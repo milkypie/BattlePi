7132f2fd1adab38c461c0333f4607f40f52db57c
James <James@10.240.198.149>
2016-03-01 12:02:47 +0000

    Removal of superfluous Active Boolean (extended class already contains said variable and related methods) and addition of a construction with that defines and initial colour

--- Battleship/src/CustomComponents/BoardSquare.java
+++ Battleship/src/CustomComponents/BoardSquare.java
@@ -7,14 +7,21 @@ import javax.swing.JPanel;
 import javax.swing.border.Border;
 
 public class BoardSquare extends JPanel {
-	public Boolean Active;
 	public Border SquareBorder;
 	public BoardSquare(int XPos,int YPos){
 		super();
 		SquareBorder = BorderFactory.createLineBorder(Color.BLACK);
-		this.Active = true;
 		this.setLayout(null);
 		this.setBackground(Color.GRAY);
+		this.setSize(20, 20);
+		this.setBorder(SquareBorder);
+		this.setLocation(XPos, YPos);
+	}
+	public BoardSquare(int XPos,int YPos,Color InitialColour){
+		super();
+		SquareBorder = BorderFactory.createLineBorder(Color.BLACK);
+		this.setLayout(null);
+		this.setBackground(InitialColour);
 		this.setSize(20, 20);
 		this.setBorder(SquareBorder);
 		this.setLocation(XPos, YPos);
