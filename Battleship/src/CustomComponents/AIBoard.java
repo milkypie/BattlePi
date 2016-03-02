d17b359141112969629a7b27bb66f0c5c074bc42
Raaj <raaj.2501@gmail.com>
2016-03-02 18:58:27 +0000

    Removed redundant variables from randomize function

--- Battleship/src/CustomComponents/AIBoard.java
+++ Battleship/src/CustomComponents/AIBoard.java
@@ -11,20 +11,17 @@ public class AIBoard extends Board{
 		super();
 	}
 
-	private int O, i, H, W;
-	private int[] HW= new int[2];
+	private int[] HW= new int[3];
 	
 	@Override
 	public void PlacePieces() {
 		// TODO Auto-generated method stub
 		
 		try {
-			O = RandomO();
-			Randomize(0,O);
-			this.Ships[0] = new Ship(this,0,new Location(HW[0],HW[1]),O);
-			O = RandomO();
-			Randomize(1,O);
-			this.Ships[1] = new Ship(this,1,new Location(HW[0],HW[1]),O);
+			Randomize(0);
+			this.Ships[0] = new Ship(this,0,new Location(HW[0],HW[1]),HW[2]);
+			Randomize(1);
+			this.Ships[1] = new Ship(this,1,new Location(HW[0],HW[1]),HW[2]);
 			//O = RandomO();
 			//Randomize(2,O);
 			//this.Ships[2] = new Ship(this,2,new Location(170,170),3);
@@ -61,71 +58,66 @@ public class AIBoard extends Board{
 		}
 	}
 	
-	public void Randomize(int shipno, int orientation) {
+	public void Randomize(int shipno) {
 		Random generator = new Random();
+		int i = generator.nextInt(4);
+		HW[2] = i;
 		if (shipno == 0) { //Destroyer
-			switch(orientation) {
+			switch(i) {
 			case 0: //Should work
 				i = (generator.nextInt(12) + 1);
-				H = (50+(i*20));
+				HW[0] = (50+(i*20));
 				i = (generator.nextInt(11) + 1);
 				W = (50+(i*20));
 				break;
 			case 2: //Should work
 				i = (generator.nextInt(12) + 1);
-				H = (50+(i*20));
+				HW[0] = (50+(i*20));
 				i = (generator.nextInt(11) + 1);
-				W = (70+(i*20));
+				HW[1] = (70+(i*20));
 				break;
 			case 1: //Should work
 				i = (generator.nextInt(11) + 1);
-				H = (70+(i*20));
+				HW[0] = (70+(i*20));
 				i = (generator.nextInt(12) + 1);
-				W = (50+(i*20));
+				HW[1] = (50+(i*20));
 				break;
 			case 3: //Should work
 				i = (generator.nextInt(11) + 1);
-				H = (50+(i*20));
+				HW[0] = (50+(i*20));
 				i = (generator.nextInt(12) + 1);
-				W = (50+(i*20));
+				HW[1] = (50+(i*20));
 				break;
 			}
 		}
 		else if (shipno == 1) { //Cruiser
-			switch(orientation) {
+			switch(i) {
 				case 0: //Should work
 					i = (generator.nextInt(10) + 1);
-					H = (90+(i*20));
+					HW[0] = (90+(i*20));
 					i = (generator.nextInt(10) + 1);
-					W = (50+(i*20));
+					HW[1] = (50+(i*20));
 					break;
 				case 2: //Should work
 					i = (generator.nextInt(10) + 1);
-					H = (50+(i*20));
+					HW[0] = (50+(i*20));
 					i = (generator.nextInt(10) + 1);
-					W = (90+(i*20));
+					HW[1] = (90+(i*20));
 					break;
 				case 1:  //Should work
 					i = (generator.nextInt(10) + 1);
-					H = (90+(i*20));
+					HW[0] = (90+(i*20));
 					i = (generator.nextInt(12) + 1);
-					W = (50+(i*20));
+					HW[1] = (50+(i*20));
 					break;
 				case 3: //Should work
 					i = (generator.nextInt(10) + 1);
-					H = (50+(i*20));
+					HW[0] = (50+(i*20));
 					i = (generator.nextInt(12) + 1);
-					W = (50+(i*20));
+					HW[1] = (50+(i*20));
 					break;
 				}
 			}
-		HW[0] = H;
-		HW[1] = W;
 	}
 	
-	public int RandomO() {
-		Random generator = new Random(); 
-		int i = generator.nextInt(4);
-		return i;
-	}
 }
