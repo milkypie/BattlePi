f39fa2960bf104b6971efded971f53a9ba1c44a9
James <James@10.240.198.149>
2016-03-04 11:17:29 +0000

    definition of done button

--- Battleship/src/Visuals/VisualControl.java
+++ Battleship/src/Visuals/VisualControl.java
@@ -10,7 +10,7 @@ public class VisualControl {
 	public static JFrame Window = new JFrame("[=] BattlePi [=]");
 	public static JPanel BasePanel = new JPanel();
 	public static final Color ShipColour = Color.PINK;
-	public static JButton HoverButton,DestroyerButton,CruiserButton,BattleshipButton,CarrierButton,RotationButton;
+	public static JButton HoverButton,DestroyerButton,CruiserButton,BattleshipButton,CarrierButton,RotationButton,DoneButton;
 	public static BoardSquare[][] PlacingIcons;
 	
 	public static void SetupVisuals(){
